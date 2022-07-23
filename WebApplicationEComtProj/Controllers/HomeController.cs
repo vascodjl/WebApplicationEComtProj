a16476544a09c5b41608fc32de7a23993450becd
vascodjl <o.email.do.vasco@gmail.com>
2022-07-23 02:44:21 +0100

    Add project files.

---
+++ WebApplicationEComtProj/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplicationEComtProj.Models;
+
+namespace WebApplicationEComtProj.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult PinturaCatObras() 
+        { 
+            return View(); 
+        }
+
+        public IActionResult EsculturaCatObras()
+        {
+            return View();
+        }
+        public IActionResult FotografiaCatObras()
+        {
+            return View();
+        }
+
+        public IActionResult PosterCatObras()
+        {
+            return View();
+        }
+        public IActionResult DesignCatObras()
+        {
+            return View();
+        }
+
+        public IActionResult MusicaCatObras()
+        {
+            return View();
+        }
+
+        public IActionResult PagPerfil()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebApplicationEComtProj/wwwroot/js/site.js
@@ -0,0 +1,20 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+const productContainers = [...document.querySelectorAll('.product-container')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+productContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
